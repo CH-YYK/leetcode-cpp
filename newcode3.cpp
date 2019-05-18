e9714333d66a119b8a090b2b2fdbaa434dd48dd0
zhuifengmu@gmail.com <kayleyang@Kayles-MacBook-Pro.local>
2019-05-18 09:07:52 -0400

    update

--- newcode3.cpp
+++ newcode3.cpp
@@ -11,16 +11,20 @@ public:
         int currmin = INT_MAX;
         N = n;
         graph = map;
+        memo = vector<int> (n * n, 0);
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(graph[i][j] != '@') continue;
-                currmin = min(currmin, bfs(i, j));
+                int res = bfs(i, j);
+                currmin = min(currmin, res);
+                if(res == 13) cout << i << j << endl;
             }
         }
         return currmin;
     }
 private:
     vector<vector<char>> graph;
+    vector<int> memo;
     int N = 0;
     int bfs(int x, int y) {
         queue<pair<int, int>> Q;
@@ -28,14 +32,19 @@ private:
         int step = 0;
         while(!Q.empty()) {
             int size = Q.size();
-            step++;
             while(size-- > 0) {
                 pair<int, int> curr = Q.front();
                 Q.pop();
+                if(memo[curr.first * N + curr.second] == 1) continue;
 
-                if(graph[curr.first][curr.second] == '*') return step;
+                cout << curr.first << curr.second << endl;
+                if(graph[curr.first][curr.second] == '*') {
+                    cout << "output:" << curr.first << curr.second << endl;
+                    return step;}
 
-                if(curr.first + 1 < N && graph[curr.first + 1][curr.second] != '#')
+                memo[curr.first*N + curr.second] = 1;
+
+                if(curr.first + 1 < N && graph[curr.first + 1][curr.second] != '#') 
                     Q.push({curr.first + 1, curr.second});
                 if(curr.first - 1 >= 0 && graph[curr.first - 1][curr.second] != '#') 
                     Q.push({curr.first - 1, curr.second});
@@ -43,6 +52,7 @@ private:
                     Q.push({curr.first, curr.second + 1});
                 if(curr.second - 1 >= 0 && graph[curr.first][curr.second - 1] != '#')
                     Q.push({curr.first, curr.second - 1});
+            step++;
             }
         }
         return INT_MAX;
@@ -61,9 +71,16 @@ int main(){
             map[i][j] = x;
         }
     }
+    cout << endl;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << map[i][j] << ' ';
+        }
+        cout << endl;
+    }
 
-    int ans = Solution().minpath(map, n);
-    cout << ans;
+    // int ans = Solution().minpath(map, n);
+    // cout << ans;
     return 0;
 };
 
