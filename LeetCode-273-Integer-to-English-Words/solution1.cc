79e11ce2b34f74592fd5ad6a84165e12159848cb
ch-yyk <yyang@dm.ai>
2021-04-12 20:53:48 -0700

    2021-04-21: modify lc273 on P52

--- LeetCode-273-Integer-to-English-Words/solution1.cc
+++ LeetCode-273-Integer-to-English-Words/solution1.cc
@@ -7,13 +7,24 @@ using namespace std;
  */
 class Solution {
 public:
-  string numberToWords(int num) {
-    return "";
-  }
+    static string numberToWords(int n) {
+        if(n == 0) return "Zero";
+        else return int_string(n).substr(1);
+    }
 private:
-  vector<string> tenToTwenty = {"Ten","Eleven", "Twelve", "Thirteen", "Fourteen",
-                                "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nighteen"};
-  vector<string> tens = {"", "", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty",
-                         "Ninety"};
-  vector<string> digits = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    static const char * const below_20[];
+    static const char * const below_100[];
+    static string int_string(int n) {
+        if(n >= 1000000000)   return int_string(n / 1000000000) + " Billion" + int_string(n - 1000000000 * (n / 1000000000));
+        else if(n >= 1000000) return int_string(n / 1000000) + " Million" + int_string(n - 1000000 * (n / 1000000));
+        else if(n >= 1000)    return int_string(n / 1000) + " Thousand" + int_string(n - 1000 * (n / 1000));
+        else if(n >= 100)     return int_string(n / 100) + " Hundred" + int_string(n - 100 * (n / 100));
+        else if(n >= 20)      return string(" ") + below_100[n / 10 - 2] + int_string(n - 10 * (n / 10));
+        else if(n >= 1)       return string(" ") + below_20[n - 1];
+        else return "";
+        }
+    }
 };
+
+const char * const Solution::below_20[] =  {"One", "Two", "Three", "Four","Five","Six","Seven","Eight","Nine","Ten", "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+const char * const Solution::below_100[] = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
