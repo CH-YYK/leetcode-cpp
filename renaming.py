f7aef4344a2a8b926cee9cdd5b1db5fa550e1559
yyk <zhuifengmu@gmail.com>
2023-04-30 17:08:55 -0700

    modify ignore list

--- renaming.py
+++ renaming.py
@@ -6,7 +6,7 @@ OLD_PATTERN = "LeetCode-[0-9]+-.+"
 
 
 # Reformat pattern `LeetCode-<num>-Problem-Name` to `<num>. Problem Name`
-def reformat_pattern(orgin: str) -> str:
+def reformat_dir_pattern(orgin: str) -> str:
     number = re.search(r'[0-9]+', orgin)
     names = re.findall(r'[A-Za-z]+', orgin)[1:]
     return ' '.join([number.group() + '.'] + names)
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     # Rename the old name to new names. If a new name already exists, then force deleting the old directory.
     for f in target_folders:
-        new_name = reformat_pattern(f)
+        new_name = reformat_dir_pattern(f)
         if os.path.exists(new_name):
             os.system("rm -rf " + f)
         else:
--- test_renaming.py
+++ test_renaming.py
@@ -1,9 +1,9 @@
-from renaming import reformat_pattern
+from renaming import reformat_dir_pattern
 
 
 def test_reformat_pattern():
     origin = "LeetCode-321-This-Is-A-Problem"
-    result = reformat_pattern(origin)
+    result = reformat_dir_pattern(origin)
     assert result == "321. This Is A Problem"
 
 
